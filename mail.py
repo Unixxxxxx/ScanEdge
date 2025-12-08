248fb12ba1bcfd2f8dc5084a7bad063b18d4ac24
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-12-08 10:44:34 +0530

    change

--- mail.py
+++ mail.py
@@ -7,7 +7,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'kritikas101traveler@gmail.com' 
-app.config['MAIL_PASSWORD'] = 'Apple123@'
+app.config['MAIL_PASSWORD'] = '*******'
 
 mail = Mail(app)
 
